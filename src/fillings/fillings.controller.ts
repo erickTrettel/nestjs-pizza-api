286c658678d34be86b73b1ab182af5d149a7bb36
Erick Trettel <ericktrettel@gmail.com>
2022-04-17 15:27:30 -0300

    [update] caching fillings

--- src/fillings/fillings.controller.ts
+++ src/fillings/fillings.controller.ts
@@ -1,18 +1,42 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  CACHE_MANAGER,
+  Controller,
+  Get,
+  Inject,
+  Post,
+} from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { CreateFillingDto } from './dto';
+import { Filling } from './entities';
 import { FillingService } from './fillings.service';
 
 @Controller('fillings')
 export class FillingsController {
-  constructor(private service: FillingService) {}
+  constructor(
+    private service: FillingService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Get()
-  getAll() {
-    return this.service.findAll();
+  async getAll() {
+    const cached: Filling[] = await this.cacheManager.get('fillings');
+
+    if (cached?.length > 0) return cached;
+
+    const fillings = await this.service.findAll();
+
+    this.cacheManager.set('fillings', fillings, { ttl: 60 * 60 }); // 1 hour
+
+    return fillings;
   }
 
   @Post()
-  createFilling(@Body() dto: CreateFillingDto) {
-    return this.service.create(dto);
+  async createFilling(@Body() dto: CreateFillingDto) {
+    const filling = await this.service.create(dto);
+
+    this.cacheManager.del('fillings');
+
+    return filling;
   }
 }
