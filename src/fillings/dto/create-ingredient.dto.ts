a549acc2e31b1b8a1fe2a2aae4c2db355886823e
Erick Trettel <ericktrettel@gmail.com>
2022-04-17 14:57:38 -0300

    [update] add validation to create filling and create ingredient dtos

--- src/fillings/dto/create-ingredient.dto.ts
+++ src/fillings/dto/create-ingredient.dto.ts
@@ -1,6 +1,9 @@
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { Filling } from '../entities';
 
 export class CreateIngredientDto {
+  @IsString() @IsNotEmpty()
   description: string;
+
   filling: Filling;
 }
